cb816f83b9b2ac3bb949b5515b84d21a3337b5aa
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-11-26 10:40:41 -0700

    Initialize git

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <div style={{
+            borderBottom: "4px solid black",
+            paddingBottom: "10px",
+            marginBottom: "12px",
+        }}>
+            Test
+        </div>
+    )
+}
+
+export default NavBar
