493fee00beaa2f5e033d5d6d58b9d50dfc01bea1
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-11-27 11:49:04 -0700

    added test button to see if i am fetching data correctly

--- src/Components/ClimbsPage.js
+++ src/Components/ClimbsPage.js
@@ -1,19 +1,37 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 function ClimbsPage() {
-    const [climb, setClimb] = useState("Climb Name")
+    const [climbs, setClimb] = useState("Climb Name")
     const [description, setDescription] = useState("Description")
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/climbs")
+        .then((res) => res.json())
+        .then((data) => {
+            setClimb(data)
+        })
+    }, [])
+
+    function logClimbs(e) {
+        e.preventDefault()
+        console.log(climbs)
+    }
+
+    // const climbsList = climbs.map((climb) => {
+    //     <p key={climb.id}>{climb}</p>
+    // })
 
     return (
         <div>
             <form>
-                <input type="text" value={climb}/>
+                <input type="text" value={climbs}/>
                 <input type="text" value={description}/>
                 <select>
                     <option>Grade</option>
                     <option>5.0</option>
                 </select>
                 <button>Submit</button>
+                <button onClick={logClimbs}>Test</button>
             </form>
         </div>
     )
