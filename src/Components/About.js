bd179c50d13d0c381b21864210219f5ce9a842ea
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-12-06 19:31:31 -0700

    cleaned up code on About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { H2, H3, P1 } from "../StyledComponents"
+import { H2, P1 } from "../StyledComponents"
 
 
 function About() {
     return (
         <div>
             <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}>
-                <H2
-                    style={{
-                        paddingRight: '50px'
-                    }}>
+                style={{ display: 'flex', justifyContent: 'center' }}>
+                <H2 style={{ paddingRight: '50px' }}>
                     About ClimbTalk.com
                 </H2>
                 <img
@@ -22,12 +16,7 @@ function About() {
                     height="600px"
                 ></img>
             </div>
-            <P1
-                style={{
-                    width: '500px',
-                    display: 'flex'
-                }}
-            >
+            <P1 style={{ width: '500px', display: 'flex' }} >
                 ClimbTalk.com is a website where any user can submit a climb that they completed, including a description of the climb and a grade they feel is appropriate.
             </P1>
         </div>
